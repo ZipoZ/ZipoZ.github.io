eb393afd9b124a5ab7a2d45808e4794dfbb01bab
Zipo-Z <the.zipoz@gmail.com>
2017-12-02 01:01:21 +0200

    website v1

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$(document).ready(function() {
+    $.get("/portfolio/main.html",
+    function(data){
+        $('#div_portfolio').html(data);
+    });
+    $.get("/portfolio/modals.html",
+    function(data){
+        $('#div_modals').html(data);
+    });
+});
